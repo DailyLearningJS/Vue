c898b55dce87202fdc598f9fa1c7b5368e0349bd
weijiyang <crawling_snail@163.com>
2019-01-18 11:49:44 +0800

    v-once v-html

--- vue-demo/src/components/HelloWorld.vue
+++ vue-demo/src/components/HelloWorld.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h3>全局components注册 使用</h3>
+    <h3>{{ msg }}</h3>
     <global-component></global-component>
+    <hr>
+    <h3>v-once 使用</h3>
+    <span>normal：{{num}}</span><span style="margin:30px;" v-once>v-once：{{num}}</span> <input type="button" @click="addNum" value="点我"/>
+    <hr>
+    <h3>v-html 使用</h3>
+    代码为:{{htmlData}}&nbsp;&nbsp;&nbsp;&nbsp;效果为：<span v-html="htmlData"></span>
+    <hr>
+    <h3></h3>
   </div>
 </template>
 
@@ -10,6 +19,17 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      num: 0,
+      htmlData: "<span style='color:red;'>这里是html语句哦~</span>"
+    }
+  },
+  methods: {
+    addNum () {
+      this.num++
+    }
   }
 }
 </script>
