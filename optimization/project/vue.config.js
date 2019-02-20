3f2c5e74dca60b1fcebf6391456ca17f7e34034a
weijiyang <crawling_snail@163.com>
2019-02-20 17:42:43 +0800

    打包配置不同模式进行区分

--- optimization/project/vue.config.js
+++ optimization/project/vue.config.js
@@ -6,17 +6,37 @@ module.exports = {
   devServer: {
     open: true
   },
-  configureWebpack: {
-    plugins: [
-      new webpack.DllReferencePlugin({
-        context: process.cwd(),
-        manifest: require('./public/vendor/vendor-manifest.json')
-      }),
-      new HtmlWebpackIncludeAssetsPlugin({
-        assets: [path.posix.join('./vendor/dll.lib.js')],
-        hash: true,
-        append: false
-      })
-    ]
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 为生产环境修改配置...
+      let arr = [
+        new webpack.DllReferencePlugin({
+          context: process.cwd(),
+          manifest: require('./public/vendor/vendor-manifest.json')
+        }),
+        new HtmlWebpackIncludeAssetsPlugin({
+          assets: [path.posix.join('./vendor/dll.lib.js')],
+          hash: true,
+          append: false
+        })
+      ]
+      config.plugins = config.plugins.concat(arr)
+      console.log('正在调用 生产模式打包配置...')
+    } else {
+      console.log('正在调用 开发模式打包配置...')
+    }
   }
+  // configureWebpack: {
+  //   plugins: [
+  //     new webpack.DllReferencePlugin({
+  //       context: process.cwd(),
+  //       manifest: require('./public/vendor/vendor-manifest.json')
+  //     }),
+  //     new HtmlWebpackIncludeAssetsPlugin({
+  //       assets: [path.posix.join('./vendor/dll.lib.js')],
+  //       hash: true,
+  //       append: false
+  //     })
+  //   ]
+  // }
 }
